2326e7686d1010543e2da51413036399477fcffb
sumathi <vijaya.sumathi@spotcues.com>
2018-07-05 13:56:30 +0530

    Changes in server js

--- server.js
+++ server.js
@@ -40,86 +40,70 @@ router.post('/login',function(req, res, next) {
        
   
   });
+  users={};
 
-  
-var chatschema=mongoose.Schema({
-    email:String,
-    message:String
-  });
-  var chat=mongoose.model('message',chatschema)
+  io.sockets.on('connection',function(socket){
+    //everytime a user connects has its own socket
+	//console.log("new user");
+	socket.on('new user',function(data,callback){
+		console.log("New user");
+		//socket.emit('select_room',data);
+		if(data in users)//if index of is not -1..i.e nickname exist
+		{
+			callback(false);
+		}
+		else
+		{
+			console.log("here");
+			callback(true);
+			socket.nickname=data;//store nickname of each user becomes clear on disconnect
+			users[socket.nickname]=socket;//key value pair as defined above
+			//nicknames.push(socket.nickname);
+			//io.sockets.emit('usernames',nicknames);//send usernames for display
+			updateNicknames();
+		}
+	});
+	socket.on('sendmessage',function(data,callback){
+		//console.log(data);
+		var msg=data.trim();
+		if(msg[0]=='@')//if thats whisper or private msg
+		{
+			msg=msg.substr(1);//start of name onwards
+			var idx=msg.indexOf(' ');
+			if(idx!==-1)
+			{
+				//check the username is valid
+				var name=msg.substr(0,idx);
+				msg=msg.substr(idx+1);
+				if(name in users)
+				{
+					users[name].emit('whisper',{msg:msg,nick:socket.nickname});
+					console.log('whispered');	
+				}
+				else
+				{
+					callback('Error! Enter a valid user');
+				}	
+			}
+			else//no actual msg part
+			{
+				callback('Error! Please enter a message for your whisper');
+			}
+		}
+		else{
+			io.sockets.emit('newmessage',{msg:msg,nick:socket.nickname});//broadcast to everyone and i too can see the msg
+			//socket.broadcast.emit('newmessage',data);//broadcast to evry1 except me
+		}
 
-users={};
-io.sockets.on('connection',function(socket){
-      var query = chat.find({});
-      query.sort('--created').limit(2).exec(function(err,docs){
-        if(err) throw err ;
-        //console.log("sending old msg");
-        socket.emit('load old msg',docs);
-       
-        
-     });
-          
-
-    socket.on('new user',function(data,callback){
-        
-        if(data in users){  // check if name exits
-            callback(false);
-        }
-        else{
-            callback(true);
-            socket.nickname =data;
-          users[socket.nickname]=socket
-            
-          updatenicknames();
-        }
-        
-    });
-
-
-
-    socket.on('send message',function(data){
-        console.log(data);
-        var msg=data.trim();
-        console.log(msg);
-        if(msg.substr(0,3)=='/w'){
-            msg= msg.substr(3);
-            var ind = msg.indexOf('');
-            if(ind!=-1)
-            {
-                var name = msg.substring(0,ind);
-                var msg = msg.substring(ind + 1);
-                if(name in users){
-                    users[name].emit('whisper',{msg:msg,nick:socket.nickname});
-                    console.log('message seny is:'+msg);
-                    console.log('whisper');
-                }
-                else{
-                    callback('error! enter a valid user.');
-                }
-                
-            }
-            else{
-                callback('error');
-            }
-        }
-        else{ console.log(msg);
-            var newmsg = new chat({email:"@",message:msg});
-            newmsg.save(function(err){
-  
-            if(err) throw err;
-            io.sockets.emit('new message',{msg:msg,nick:socket.nickname});
-            });
-        }
-
-
-    });
-
-    socket.on('disconnect',function(data){  
-      if(!socket.nickname) return ;
-      delete users[socket.nickname]
-       });
-    function updatenicknames(){
-        io.sockets.emit('usernames',Object.keys(users));
-    }
+	});
+	function updateNicknames(){
+		io.sockets.emit('usernames',Object.keys(users));//sending socket does not make sense
+	}
+	//whenever user disconnect he/she should be removed from the list
+	socket.on('disconnect',function(data){
+		if(!socket.nickname)//when the user has no nickname 
+			return;
+		delete users[socket.nickname];
+		updateNicknames();
+	});
 });
-
